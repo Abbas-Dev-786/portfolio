e75ed6d729fa362748f73f6d7c122cfceab28f23
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-02-14 17:49:46 +0530

    added google analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+
+// initialize google analytics
+ReactGA.initialize("G-SM2T6YE4VT");
+
+// Send pageview with a custom path
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
